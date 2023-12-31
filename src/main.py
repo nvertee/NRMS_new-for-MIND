2864f828e0631bdf0fcd65c25754f91e6381a974
Van Tho Nguyen <102587827+nvertee@users.noreply.github.com>
2024-01-01 02:38:05 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -46,7 +46,8 @@ def process_batch_data(behavior_data, index):  # padding and masking
     data = []
     for i in range(len(index)):
         print(index[i])
-    data = behavior_data[index]
+        data = np.append(data, behavior_data[index[i]])
+    print(data[1])
     history = data[:, 1]
     recent = data[:, 2]
     batch_user_history, user_history_mask_selfattn, user_history_mask_attn = pad_masking(history)
