6ec909d1928e4342dafa86a01c60ec2565019132
Van Tho Nguyen <102587827+nvertee@users.noreply.github.com>
2024-01-01 02:06:42 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -79,7 +79,8 @@ def train(model, optimizer, loader, train_data, epoch):
     summ = []
     for step, (batch_x, batch_y) in enumerate(loader):  # batch_x: index of batch data
         print('Epoch: ', epoch, ' | Iteration: ', step + 1, '/' + str(num_iterations))
-        processed_data = process_batch_data(train_data, batch_x)
+        index = torch.tensor(int(batch_x))
+        processed_data = process_batch_data(train_data, index)
         batch_user_history = processed_data[0]
         batch_user_short = processed_data[1]
         user_history_mask_selfattn = processed_data[2]
